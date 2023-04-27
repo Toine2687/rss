8ffc3ff492a22bae2757d80c4d8ccd7e5b6a0045
Toine2687 <ant.petit@pm.me>
2023-04-27 16:59:33 +0200

    settings en cours

---
+++ controllers/testCtrl.php
@@ -0,0 +1,4 @@
+<?php
+// include __DIR__ . '/../views/header.php';
+include __DIR__ . '/../views/settings.php';
+include __DIR__ . '/../views/footer.php';
--- views/checkbox.php
+++ views/checkbox.php
@@ -1,6 +1,15 @@
-<?php foreach (TOPICS as $key => $value) {  ?>
-    <input type="checkbox" name="userTopic[]" value="<?=$value?>" id="<?=$key?>">
-    <label class="btn btn-outline-primary"  for="<?=$key?>"><?=$key?></label>
-<?php } ?>
-<P><?= $error['topics'] ?? ""?></P>
+<ul>
+    <?php
+    require_once __DIR__ . '/../config/const.php';
 
+    foreach (TOPICS as $key => $value) {  ?>
+        <li>
+            <div class="checkbox">
+
+                <input type="checkbox" name="userTopic[]" value="<?= $value ?>" id="<?= $key ?>">
+                <label class="btn btn-outline-primary" for="<?= $key ?>"><?= $key ?></label>
+            </div>
+        </li>
+    <?php } ?>
+</ul>
+<P><?= $error['topics'] ?? "" ?></P>
