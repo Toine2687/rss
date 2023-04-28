a516f14195f21553ba51e27895976f418f90c792
Toine2687 <ant.petit@pm.me>
2023-04-28 12:07:34 +0200

    parameters.php OK

--- config/cookies.php
+++ config/cookies.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once __DIR__ . '/../config/const.php';
 
 
@@ -7,8 +7,7 @@ $displayTopics = (array)json_decode($_COOKIE['topics']);
 $displayTopics = array_intersect(TOPICS, $displayTopics);
 
 
-if (in_array($displayNB, ARTICLE_NB) == true){
 $displayNb = ($_COOKIE['articleNb']);
-}else {
+if (in_array($displayNb, ARTICLE_NB) == false) {
     $displayNB = 6;
 }
--- controllers/formCtrl.php
+++ controllers/formCtrl.php
@@ -18,19 +18,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         setcookie('articleNb', $articleNb, time() + 31000000, '/');
     }
 }
-include __DIR__ . '/../views/header.php';
-
-
-?>
-
-<form method="post">
-    <?php
-    include __DIR__ . '/../views/checkbox.php';
-    include __DIR__ . '/../views/select.php';
-    ?>
-    <button type="submit">PREFERENCE</button>
-</form>
-<?php
+// include __DIR__ . '/../views/header.php';
+include __DIR__ . '/../views/settings.php';
 include __DIR__ . '/../views/footer.php';
-
-
