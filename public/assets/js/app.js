b36fcb90df88bf4ae2e6ee5d3e32764e73643598
Toine2687 <ant.petit@pm.me>
2023-05-02 10:57:45 +0200

    preventDefault OK

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,19 +6,23 @@ darkBtn.addEventListener('click', () => {
 
 //recuper les checkboxs
 const checkboxArray = document.querySelectorAll('[type=checkbox]')
+const settingsBtn = document.getElementById('settingsBtn')
 
 //boucler sur les checkboxs
 checkboxArray.forEach((checkbox) => {
 	//Event sur le changement de status des checkbox
-	checkbox.addEventListener('change', (event) => {
+	checkbox.addEventListener('change', () => {
 		//Si plus de 3 checkbox, limiter
 		if (document.querySelectorAll('[type=checkbox]:checked').length > 3) {
 			checkbox.checked = false
 			document.getElementById('checkboxInfo').innerHTML = 'Vous ne pouvez plus choisir de sujet'
-
-			//sinon prévenir
-		} else {
-			document.getElementById('checkboxInfo').innerHTML = 'Vous devez choisir 3 sujets'
 		}
 	})
+})
+//Si moins de 3 choix, empecher validation
+settingsBtn.addEventListener('click', (event) => {
+	if (document.querySelectorAll('[type=checkbox]:checked').length < 3) {
+		document.getElementById('checkboxInfo').innerHTML = 'Vous devez choisir 3 sujets'
+		event.preventDefault()
+	}
 })
