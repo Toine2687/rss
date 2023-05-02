072beef67b058979d46f810f24cbb8cbe2723925
Nathan <demory.nathan@gmail.com>
2023-05-02 09:42:42 +0200

    for cookie

--- views/mono.php
+++ views/mono.php
@@ -3,6 +3,23 @@
 <div class="fullWidthContainer">
     <?php
     $testxml = simplexml_load_file(TOPICS[$display]);
+    $monoDisplay = $testxml->channel->item;
+
+    var_dump(count($testxml->channel->item));
+
+    // for ($i = 0; $i< $displayNb; $i++) {
+    //     $newDate = new DateTime($monoDisplay[$i]->pubDate);
+    //     $fmtDate = new IntlDateFormatter('fr_FR');
+    //     $fmtDate->setPattern('EEEE F LLLL yyyy à HH:mm');
+    //     echo '
+    //             <div class="article">
+    //                 <h2><a href="' . $monoDisplay[$i]->link . '">' . $monoDisplay[$i]->title . '</a></h2>
+    //                 <p class="date">' . $fmtDate->format($newDate) . '</p>
+    //                 <p class="description">' . $monoDisplay[$i]->description . '</p>
+    //                 <p class="link"><i><a href="' . $monoDisplay[$i]->link . '">Lire la suite...</a></i></p>
+    //             </div>';
+    // }
+
     foreach ($testxml->channel->item as $item) {
         $newDate = new DateTime($item->pubDate);
         $fmtDate = new IntlDateFormatter('fr_FR');
